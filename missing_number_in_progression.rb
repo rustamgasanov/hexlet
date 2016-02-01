260a833d79b392ab96780c84042b12f5834bad01
Rustam Gasanov <rustamagasanov@gmail.com>
2016-02-01 17:12:50 +0100

    Updated missing number in progression

--- missing_number_in_progression.rb
+++ missing_number_in_progression.rb
@@ -4,8 +4,8 @@
 
 def solution(arr)
   map = {}
-  (0..arr.size - 2).each_with_index { |el, i| map[i] = (arr[i] - arr[i + 1]).abs }
-  max = map.max_by { |_,v| v }
-  (arr[0] < arr[1]) ? (arr[max[0]] + max[1] / 2) : (arr[max[0]] - max[1] / 2)
+  (0..arr.size - 2).each_with_index { |el, i| map[i] = arr[i] - arr[i + 1] }
+  max = map.max_by { |_,v| v.abs }
+  arr[max[0]] - max[1] / 2
 end
 
