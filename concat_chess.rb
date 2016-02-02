2971d5f1697a5e280589c6264bac192893c58e26
Rustam Gasanov <rustamagasanov@gmail.com>
2016-02-02 12:38:25 +0100

    Added concat chess

---
+++ concat_chess.rb
@@ -0,0 +1,7 @@
+# Concatenate 2 strings by characters, one by one. In other words, take the 1st char from the first string, then 1st char from the second string, then 2nd char from the first string, then 2nd char from the second string, and so on. If one string ends before the other, just continue with the remaining characters.
+#
+# Example: "awbxcydz" == solution("abcd", "wxyz")
+
+def solution(s1, s2)
+  s1.split('').zip(s2.split('')).flatten.join
+end
